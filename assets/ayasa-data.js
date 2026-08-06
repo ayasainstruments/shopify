9e7ccfed59fb67344343eecfe4862edb34708faf
roy <roy@ayasainstruments.com>
2026-08-06 11:45:09 +0000

    studio: 2 changes
    - converted lukasz-dembinski-d-aegean-18-1.mp4
    - fetched lukasz-dembinski-d-aegean-18-1.mp4 from Instagram
    
    By: roy@ayasainstruments.com

--- assets/ayasa-data.js
+++ assets/ayasa-data.js
@@ -162,7 +162,8 @@ const ARTISTS = {
     clips: [
       { name: "F# Low Pygmy 21", mode: "F# minor pentatonic · fully extended", handle: "fis-low-pygmy-21-ember-steel", file: AYASA_ASSET("lukasz-dembinski-fis-low-pygmy-21-1.mp4") },
       { name: "E Amara 20", mode: "E Celtic minor · fully extended", handle: "e-amara-20-ember-steel", file: AYASA_ASSET("lukasz-dembinski-e-amara-20-1.mp4") },
-      { name: "E Amara 20", mode: "E Celtic minor · fully extended", handle: "e-amara-20-ember-steel", file: AYASA_ASSET("lukasz-dembinski-e-amara-20-2.mp4") }
+      { name: "E Amara 20", mode: "E Celtic minor · fully extended", handle: "e-amara-20-ember-steel", file: AYASA_ASSET("lukasz-dembinski-e-amara-20-2.mp4") },
+      { name: "D Aegean 18", mode: "D Lydian", handle: "d-aegean-18-ember-steel", file: AYASA_ASSET("lukasz-dembinski-d-aegean-18-1.mp4") }
     ],
     plays: [
       { name: "C Ashakiran 17", mode: "", handle: "c-ashakiran-17-ember-steel",
@@ -655,7 +656,8 @@ const MODELS = [
       { artist: "Malte Marten", file: AYASA_ASSET("malte-marten-d-aegean-18-1.mp4") },
       { artist: "Malte Marten", file: AYASA_ASSET("malte-marten-d-aegean-18-2.mp4") },
       { artist: "Johann Immanuel", file: AYASA_ASSET("johann-immanuel-d-aegean-18-1.mp4") },
-      { artist: "Vybeshift", file: AYASA_ASSET("vybeshift-d-aegean-18-1.mp4") }
+      { artist: "Vybeshift", file: AYASA_ASSET("vybeshift-d-aegean-18-1.mp4") },
+      { artist: "Łukasz Dembiński", file: AYASA_ASSET("lukasz-dembinski-d-aegean-18-1.mp4") }
     ]
   },
   {
